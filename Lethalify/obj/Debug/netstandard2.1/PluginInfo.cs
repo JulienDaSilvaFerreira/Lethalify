e8e0c9be8ed9106e751daf50dd213257fd244a39
JulienDaSilvaFerreira <julien-da-silva-ferreira@hotmail.fr>
2024-02-28 20:14:35 +0900

    Initial commit

---
+++ Lethalify/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Lethalify
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "Lethalify";
+        public const string PLUGIN_NAME = "Lethalify";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
